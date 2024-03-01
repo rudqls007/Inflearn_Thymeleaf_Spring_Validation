eb9b32cadba157f98f7af29ae0a3f9c543a9c41b
rudqls007 <rudqls007@gmail.com>
2024-03-01 13:19:18 +0900

    Bean Validation - Form 전송 객체 분리 - 개발

---
+++ src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
@@ -0,0 +1,24 @@
+package hello.itemservice.web.validation.form;
+
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+public class ItemSaveForm {
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    @NotNull
+    @Max(value = 9999, groups = SaveCheck.class)
+    private Integer quantity;
+}
